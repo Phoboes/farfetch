08f03a8168683a4dc2b659959aaf9a839ca96273
Kane <Phoboes@live.com>
2020-09-20 23:49:17 +1000

    scrape task house cleaning.

--- tasks/filter.js
+++ tasks/filter.js
@@ -1,7 +1,7 @@
 const $ = require('cheerio');
 const PageFilter = {
 
-    getMetric: function(html){
+    getMetric: function (html) {
         const metrics = $("[title='Metric']", html);
         const parsedVals = [];
         for (let i = 0; i < metrics.length; i++) {
@@ -30,7 +30,7 @@ const PageFilter = {
         };
     },
 
-    getThisPokemonCard: function(html, pokemon){
+    getThisPokemonCard: function (html, pokemon) {
         const cardList = $('.pi-section-navigation li div', html);
         // debugger
         if (cardList.length > 0) {
@@ -44,7 +44,7 @@ const PageFilter = {
                     $('.pi-title', currentCard)[0].children[0].data.trim() === pokemon.name
                 ) {
                     return currentCard;
-                } else if ( i + 1 === cardList.length ){
+                } else if (i + 1 === cardList.length) {
                     return $('.pi-section-content', html)[0];
                 }
             }
@@ -53,7 +53,7 @@ const PageFilter = {
         }
     },
 
-    getTypes: function(html){
+    getTypes: function (html) {
         const typeList = $(".image[title*=' type']", html);
         const cleanedTypes = [];
         for (let i = 0; i < typeList.length; i++) {
@@ -62,18 +62,38 @@ const PageFilter = {
         return cleanedTypes.length > 1 ? `${cleanedTypes[0]}/${cleanedTypes[1]}` : cleanedTypes[0];
     },
 
-    getAbilities: function(html){
+    // TODO: Transition these to models and an uploader and validate the data.
+    getImages: function (html) {
+        const images = $('.pi-smart-group-body .image img', html);
+        const imageCollection = [];
+        imageCollection[0] = $('.pi-image-thumbnail', html)[0].attribs.src;
+        imageCollection[1] = $('figure img', html)[0].attribs.srcset.match(/\s(https:\/\/.+)\s2/)[1];
+        // Try for all the evolution sprites on the page, there aren't classes so we need annoying selectors to find what we need
+        //  Ignore the img if it has the "Shape" tag -- it's a weird dinosaur thing
+        //  Ignore the shape if it's under 20px wide -- It's probably a footprint sprite.
+        for (let i = 0;
+            (i < images.length); i++) {
+            const sprites = [];
+            if (!/(Shape)/.test(images[i].attribs.alt) && images[i].attribs.width > 20) {
+                sprites.push(images[i].attribs["data-src"])
+            }
+            imageCollection.push(sprites);
+        }
+
+        return imageCollection;
+    },
+
+    getAbilities: function (html) {
         const abilities = [];
+        // The only properly labelled ability item is a buried sibling to the container, so we gotta dig... up. Then back down. But not too down.
         const abilityList = $("[title*=' Abilities']", html).parent().parent().children().find('a').not('sup a').not('h3 a');
         for (let i = 0; i < abilityList.length; i++) {
-            // debugger
             abilities.push(abilityList[i].attribs.title);
         }
         return abilities;
     },
 
-    getIndex: function(html){
-        // debugger
+    getIndex: function (html) {
         return parseInt($('div[data-source="ndex"]', html)[0].children[0].data);
     },
 
@@ -81,17 +101,39 @@ const PageFilter = {
         return $('[title^="Generation"]', html)[0].children[0].data;
     },
 
-    getRes(input){
-        console.log(input);
-        return input;
-    },
-
-    getUrlData(urls){
+    getUrlData(urls) {
+        // Traverses the list of links in a given table rom for the first with a title which contains a name & href.
         for (let i = 0; i < urls.length; i++) {
             if (urls[i].attribs.title !== undefined) {
                 return urls[i];
             }
         }
+    },
+
+    //  TODO Descriptions are broken frequently by terrible formatting. This needs a lot of tweaking.
+    getDescription(html) {
+        desc = $('.pokedex_entry p', html);
+        // Sword and shield pokemon currently have broken/missing descriptions, this code is mostly to avoid them.
+        if (desc.length) {
+            if (desc.children !== undefined || desc.children.length === 0) {
+                if (desc[0].children[0] !== undefined) {
+                    return desc[0].children[0].data;
+                }
+            }
+        } else {
+            return '';
+        }
+
+    },
+
+    logResponse(pokemon, url) {
+        console.log(`Index: ${ pokemon.index }, ${ pokemon.name }, ${ `${url}/wiki/${pokemon.name}` }, ${ pokemon.generation }`);
+        console.log(pokemon.images[1]);
+        console.log(pokemon.elementType);
+        console.log(`Weight: ${pokemon.weight}, height: ${pokemon.height}`);
+        console.log(`Moves: ${pokemon.moves.length}`);
+        console.log(pokemon.description);
+        console.log('-----------------------');
     }
 
 };
--- tasks/scrape.js
+++ tasks/scrape.js
@@ -2,153 +2,78 @@ Pokemon = require('../models/pokemonModel');
 const filter = require('./filter')
 const $ = require('cheerio');
 const rp = require('request-promise');
-// const fetch = require("node-fetch");
 const { html } = require('cheerio');
 
 const baseURL = 'https://pokemon.fandom.com';
 const pokeIndex = '/wiki/List_of_Pokémon';
-let timer = 0;
 const genNum = 1;
 
-// Begin timer for total task.
-const timerID = setInterval(() => {
-    timer += 100;
-  }, 100);
+rp(baseURL + pokeIndex)
+  .then(function (html) {
+    console.log('--------Starting--------');
+    const genTables = $('.wikitable', html);
+    cycleTables(genTables);
 
-rp( baseURL + pokeIndex )
-  .then(function(html){
-    console.log('--------Success--------');
-    const genTables = $( '.wikitable', html );
-    console.log( `Tables/Generations found: ${ genTables.length }.` );
-    console.log('-----------------------');
+    function cycleTables(table) {
+      let timer = 0;
+      const timerID = setInterval(() => {
+        timer += 100;
+      }, 100);
 
-    cycleTables();
-
-    async function cycleTables (){
-      for( let i = (genNum - 1); i < genTables.length; i++ ){
-        await parseTable( genTables[i] );
-        console.log('-----------------------');
+      for (let i = (genNum - 1); i < table.length; i++) {
+        await parseTable(table[i]);
         console.log('-----------------------');
         console.log(`Table ${ i + 1} done, ${ timer / 60000 } minutes.`);
         console.log('-----------------------');
-        console.log('-----------------------');
-
       }
       console.log(`Complete. Time: ${ timer / 60000 } minutes.`);
       clearInterval(timerID);
-    }
-
-    async function parseTable ( gen ){
-
-      // Iterating tables for each generation. The first TR is a header, so we skip it with let 1.
-      for( let i = 1; i < $( 'tr', gen ).length; i++ ){
-
-        const singlePokemonRow = $( 'tr', gen )[i];
-        const urls = $( 'a', singlePokemonRow );
-        const nameUrl = filter.getUrlData( urls );
-        const nameTag = nameUrl.attribs.title;
-
-        // Save the partial URL (ie /wiki/bulbasaur/ for deeper page scraping)
-        const pokeUrl = nameUrl.attribs.href;
-        const newPokemon = new Pokemon;
-
-        // Going deeper:
-          // Access the single page view of each pokemon we scrape using the /wiki/{pokemonName} above
-        const singlePageScrape = ( wikiUrl ) => {
-          return new Promise( resolve =>
-            {
-              rp( baseURL + wikiUrl ).then(( singleViewHtml ) => {
-                newPokemon.name = nameTag;
-                const parentCard = filter.getThisPokemonCard(singleViewHtml, newPokemon);
-                const metrics = filter.getMetric(parentCard);
-                newPokemon.weight = metrics.weight;
-                newPokemon.height = metrics.height;
-
-                newPokemon.moves = filter.getAbilities( parentCard );
-                newPokemon.elementType = filter.getTypes(parentCard);
-                newPokemon.index = filter.getIndex(parentCard);
-                newPokemon.generation = filter.getGen( parentCard );
-                // images for later use:
-                newPokemon.images.cardImage = $('.pi-image-thumbnail', parentCard)[0].attribs.src;
-                newPokemon.images.cardSprite = $('h2 .image img', parentCard)[0].attribs["data-src"];
-
-                //  TODO Descriptions are broken frequently by terrible formatting. This needs a lot of tweaking.
-                desc = $('.pokedex_entry p', singleViewHtml);
-                // Sword and shield pokemon currently have broken/missing descriptions, this code is mostly to avoid them.
-                if( desc.length ){
-                  if (desc.children !== undefined || desc.children.length === 0) {
-                    if (desc[0].children[0] !== undefined) {
-                      newPokemon.description = desc[0].children[0].data;
-                    }
-                  }
-                } else {
-                  newPokemon.description = '';
-                }
-
-
-                const images = $('.pi-smart-group-body .image img', singleViewHtml);
-
-                // Try for all the evolution sprites on the page, there aren't classes so we need annoying selectors to find what we need
-                  //  Ignore the img if it has the "Shape" tag -- it's a weird dinosaur thing
-                  //  Ignore the shape if it's under 20px wide -- It's probably a footprint sprite.
-                for( let i = 0; ( i < images.length ); i++ ){
-                  if (!/(Shape)/.test(images[i].attribs.alt) && images[i].attribs.width > 20 ){
-                    newPokemon.images.push(images[i].attribs["data-src"])
-                  }
-                }
-                resolve( newPokemon );
-              })
-            })
-        }
-
-
-        const result = await singlePageScrape( pokeUrl );
-        console.log(`Index: ${ newPokemon.index }, ${ newPokemon.name }, ${ baseURL + pokeUrl }, ${ newPokemon.generation }`);
-        console.log( newPokemon.images.cardImage );
-        console.log( newPokemon.elementType );
-        console.log(`Weight: ${newPokemon.weight}, height: ${newPokemon.height}` );
-        console.log( `Moves: ${newPokemon.moves.length}` );
-        console.log(newPokemon.description);
-        console.log('-----------------------');
-        }
-
     };
 
+    async function parseTable(gen) {
+      // Iterating tables/rows for each generation. The first TR is a header, so we skip it with let 1.
+      for (let i = 1; i < $('tr', gen).length; i++) {
 
+        const singlePokemonRow = $('tr', gen)[i];
+        const urls = $('a', singlePokemonRow);
+        const nameUrl = filter.getUrlData(urls);
+        const nameTag = nameUrl.attribs.title;
+        // Save the partial URL (ie /wiki/bulbasaur/ for deeper page scraping)
+        const pokeUrl = nameUrl.attribs.href;
+
+        // Going deeper:
+        // Access the single page view of each pokemon we scrape using the /wiki/{pokemonName} above
+        const singlePageScrape = async (wikiUrl) => {
+          return new Promise(resolve => {
+            rp(baseURL + wikiUrl).then((singleViewHtml) => {
+              const newPokemon = new Pokemon;
+              newPokemon.name = nameTag;
+              const parentCard = filter.getThisPokemonCard(singleViewHtml, newPokemon);
+              const metrics = filter.getMetric(parentCard);
+              newPokemon.weight = metrics.weight;
+              newPokemon.height = metrics.height;
+              newPokemon.moves = filter.getAbilities(parentCard);
+              newPokemon.elementType = filter.getTypes(parentCard);
+              newPokemon.index = filter.getIndex(parentCard);
+              newPokemon.generation = filter.getGen(parentCard);
+
+              // images for later use:
+              newPokemon.images = filter.getImages(parentCard);
+              newPokemon.description = filter.getDescription(singleViewHtml);
+
+              resolve(newPokemon);
+              filter.logResponse(newPokemon, baseURL);
+            })
+          })
+        };
+
+        await singlePageScrape(pokeUrl);
+      }
+    };
   }) // End of successful response
-  .catch(function(err){
+  .catch(function (err) {
     //handle error
     console.log("You done messed up A-Aron!");
-    console.log( err );
+    console.log(err);
     clearInterval(timerID);
   }); // End of error handling
-
-
-
-
-
-// NEW POKEMON TEMPLATE
-// exports.new = function (req, res) {
-//     var pokemon = new Pokemon();
-//     pokemon.name = req.body.name ? req.body.name : pokemon.name;
-//     pokemon.index = req.body.index;
-//     pokemon.height = req.body.height;
-//     pokemon.weight = req.body.weight;
-//     pokemon.elementType = req.body.elementType;
-//     // pokemon.images = req.body.images;
-//     pokemon.moves = req.body.moves;
-//     pokemon.description = req.body.description;
-    
-//     // save the pokemon and check for errors
-//     pokemon.save(function (err) {
-//         if (err)
-//             res.json(err);
-            
-//             res.json({
-//             message: 'New pokemon created!',
-//             data: pokemon
-//         });
-//     });
-// };
-// console.log( `Complete. Time: ${ timer / 60000 }` );
-// clearInterval(timerID);
